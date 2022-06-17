038bdae0432c20dac1a99bf7f489a354d52a2245
Josias Escobar Denone Junior <JEDJA@rbservicos.corporate>
2022-06-17 19:15:11 -0300

    Inclusão do módulo 2

---
+++ module_2/main.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+# A primeira linha indica para versões anteriores a 3.0.0
+# que as String possuem caracteres especiais e precisam sem 
+# apresentadas na codificação UTF-8
+# Versões superiores possuem essa função nativa
+
+print('****************************************')
+print('*** School of Net - Caixa Eletrônico ***')
+print('****************************************')
+
+# Caracter especial sendo apresentado pela tabela ASCII
+#print(chr(1)) 
+
