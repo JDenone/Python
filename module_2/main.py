1dd5af9df9097582f54d0cec5f6e7de5182ca290
Josias Escobar Denone Junior <JEDJA@rbservicos.corporate>
2022-12-27 17:04:30 -0300

    modulo de introdução à lógica do projeto

--- module_2/main.py
+++ module_2/main.py
@@ -1,14 +1,30 @@
-# coding=utf-8
-
-# A primeira linha indica para versões anteriores a 3.0.0
-# que as String possuem caracteres especiais e precisam sem 
-# apresentadas na codificação UTF-8
-# Versões superiores possuem essa função nativa
+import getpass
 
 print('****************************************')
 print('*** School of Net - Caixa Eletrônico ***')
 print('****************************************')
 
-# Caracter especial sendo apresentado pela tabela ASCII
-#print(chr(1)) 
+#   o input recebe os dados da iteração e salva como uma string e é necessário ter uma variável para receber 
+# os dados da iteração
+account_typed = input('Digite sua conta: ')
+password_typed = getpass.getpass('Digite sua senha: ')
+print(account_typed)
+print(password_typed)
 
+accounts_list = {
+    '0001-02': {
+        'password' : '123456',
+        'name' : 'Cliente 1',
+        'value' : 0
+    },
+    '0002-02' : {
+        'password' : '456123',
+        'name' : 'Cliente 2',
+        'value' : 10
+    }
+}
+
+if account_typed in accounts_list and password_typed == accounts_list[account_typed]['password'] : 
+    print('Conta válida')
+else: 
+    print('Conta inválida')
